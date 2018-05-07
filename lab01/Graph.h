013bd46ffa09512e9a529a9de348c7d2df9e307a
Layika <mwoleksy@gmail.com>
2018-05-07 21:56:41 +0200

    WIP: added documentation in Graph.h

--- lab01/Graph.h
+++ lab01/Graph.h
@@ -3,25 +3,29 @@
 #include <iostream>
 #include "Matrix.h"
 
-/* Class Graph has a matrix field where it can store any type of matrix as class Matrix.
- * It also handles translation of the matrix to all different types */
+/* Class Graph has a matrix field where it can store any type of matrix as class Matrix *
+ * Base class for oprating on graphs                                                    */
 
 class Graph {
 public:
+  // Conctructor for graphs, only creates a new matrix, but doesn't fill it
+  // In order to get a matrix we need to read from file or create a random matrix
   Graph() { matrix = new Matrix; }
 
-  // TODO: wywalić do readFile i użyć setData
+  // Reads matrix from file. After a read matrix will be updated
   void readFile(std::string fileName) { matrix->readFile(fileName); }
+
+  // Wrapper function converting any type to the type we want
+  // It handles calling the right function (see private functions)
+  void convertMatrix(RepresentationType to);
 
   // Wrapper functions for handling saving graph to file in adjacency list and debug print
   void saveGraph(std::string fileName) { convertMatrix(AdjacencyList); matrix->saveAdjList(fileName); }
   void print() const { matrix->print(); }
 
-  void convertMatrix(RepresentationType to);
-
   // TODO: random graph
   // I think we should make a wrapper function too, but I'm not sure, I guess it will be consistent with other ones
-  
+
 private:
   Matrix* matrix;
 
