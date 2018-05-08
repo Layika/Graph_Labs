d6010e4c886aeb27aa46791cee9042b7daa5f79a
Layika <mwoleksy@gmail.com>
2018-05-08 15:48:52 +0200

    Added all tests

--- lab01/Tests.cpp
+++ lab01/Tests.cpp
@@ -26,4 +26,59 @@ int main() {
   adjacencyListGraph.saveGraph("graph.txt");
   std::cout << "Graph ready to plot" << std::endl;
   std::cout << std::endl << std::endl;
+
+
+  // ADJACENCY MATRIX GRAPH
+  std::cout << "ADJACENCY MATRIX GRAPH" << std::endl << std::endl;
+  std::cout << "Creating adjacency matrix graph..." << std::endl;
+  Graph adjacencyMatrixGraph;
+
+  std::cout << "Reading adjacency matrix from file:" << std::endl;
+  adjacencyMatrixGraph.readFile("adjmatrix.txt");
+  adjacencyMatrixGraph.print();
+  std::cout << std::endl;
+
+  std::cout << "Converting adjacency matrix to incidence matrix:" << std::endl;
+  adjacencyMatrixGraph.convertMatrix(IncidenceMatrix);
+  adjacencyMatrixGraph.print();
+  std::cout << std::endl;
+
+  std::cout << "Converting incidence matrix to adjacency matrix:" << std::endl;
+  adjacencyMatrixGraph.convertMatrix(AdjacencyMatrix);
+  adjacencyMatrixGraph.print();
+  std::cout << std::endl;
+
+  std::cout << "Saving graph to file graph.txt..." << std::endl;
+  adjacencyMatrixGraph.saveGraph("graph.txt");
+  std::cout << "Graph ready to plot" << std::endl;
+  std::cout << std::endl << std::endl;
+
+
+  // INCIDENCE MATRIX GRAPH
+  std::cout << "INCIDENCE MATRIX GRAPH" << std::endl << std::endl;
+  std::cout << "Creating incidence matrix graph..." << std::endl;
+  Graph incidenceMatrixGraph;
+
+  std::cout << "Reading incidence matrix from file:" << std::endl;
+  incidenceMatrixGraph.readFile("incmatrix.txt");
+  incidenceMatrixGraph.print();
+  std::cout << std::endl;
+
+  std::cout << "Converting incidence matrix to adjacency list:" << std::endl;
+  incidenceMatrixGraph.convertMatrix(AdjacencyList);
+  incidenceMatrixGraph.print();
+  std::cout << std::endl;
+
+  std::cout << "Converting adjacency list to incidence matrix:" << std::endl;
+  incidenceMatrixGraph.convertMatrix(IncidenceMatrix);
+  incidenceMatrixGraph.print();
+  std::cout << std::endl;
+
+  std::cout << "Saving graph to file graph.txt..." << std::endl;
+  incidenceMatrixGraph.saveGraph("graph.txt");
+  std::cout << "Graph ready to plot" << std::endl;
+  std::cout << std::endl << std::endl;
+
+  // RANDOM GRAPHS
+  std::cout << "" << std::endl << std::endl;
 }
