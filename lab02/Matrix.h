c59e6ad5fae6db5282d779ffcefc241d995fb557
Mikołaj Skawiński <>
2018-05-16 13:27:04 +0200

    implement function for returning degree seq

--- lab02/Matrix.h
+++ lab02/Matrix.h
@@ -29,7 +29,7 @@ public:
   unsigned int getColumns(unsigned int row) const { return data[row].size(); }
   int getElement(unsigned int row, unsigned int col) const { return data[row][col]; }
   // TODO: segfault
-  std::vector<int> getDegreeSequence();
+  std::vector<unsigned int> getDegreeSequence();
   
   // Setters for matrix
   void saveData(std::vector<std::vector<int>> newData) { data.clear(); data = newData; }
